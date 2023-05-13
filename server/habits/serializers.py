3a89277ae0f7ad4574e0a18e648c1756e2b1896c
joaovictorgarciacoelho@gmail.com <joaovictorgarciacoelho@gmail.com>
2023-05-13 08:14:53 -0300

    Adding Habit Serializer

---
+++ server/habits/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import Habit
+
+class HabitSerializer(serializers.ModelSerializer):
+
+  class Meta:
+      model = Habit
+      fields = ('id','name', 'start_at')
