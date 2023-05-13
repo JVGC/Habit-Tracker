29109b1e6d2b429fd04ac7f6a7dd60bbc10e5a43
joaovictorgarciacoelho@gmail.com <joaovictorgarciacoelho@gmail.com>
2023-05-13 08:22:02 -0300

    Adding ListHabits route

--- server/habits/urls.py
+++ server/habits/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 # this name "urlpatterns" cannot be change to snake case
 urlpatterns = [
-    path("add/", views.AddNewHabit.as_view(), name="AddNewHabit")
+    path("add", views.AddNewHabit.as_view(), name="AddNewHabit"),
+    path("", views.ListHabits.as_view(), name="ListHabits")
 ]
