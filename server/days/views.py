22f8b5994602c1d6b6eaba1a54b15a496d160e1c
joaovictorgarciacoelho@gmail.com <joaovictorgarciacoelho@gmail.com>
2023-05-15 11:35:54 -0300

    Adding date validation do AddNewDay View

--- server/days/views.py
+++ server/days/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics
+from rest_framework import generics, response
 from .serializers import DaySerializer
 from .models import Day
 
@@ -6,4 +6,15 @@ class AddNewDay(generics.CreateAPIView):
     queryset = Day.objects.all()
     serializer_class = DaySerializer
 
-# Create your views here.
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        does_date_exist = Day.objects.filter(date=request.data['date'])
+        if does_date_exist:
+          return response.Response(status=400, data={
+             "date": "This date already has a correspondent Day Object"
+          })
+
+        return super().create(request, *args, **kwargs)
+
