5f0d68328203e6496744069ef0d8c9d49eb6a251
joaovictorgarciacoelho@gmail.com <joaovictorgarciacoelho@gmail.com>
2023-05-15 10:55:35 -0300

    Configuring days app on project

--- server/habitTracker/settings.py
+++ server/habitTracker/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'rest_framework',
     'habits.apps.HabitsConfig',
+    'days.apps.DaysConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- server/habitTracker/urls.py
+++ server/habitTracker/urls.py
@@ -38,5 +38,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('habits/', include("habits.urls"))
+    path('habits/', include("habits.urls")),
+    path('days/', include("days.urls"))
 ]
