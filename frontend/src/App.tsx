408fc0238a1b0d943143fcb9c84bec40fc5e5a71
joaovictorgarciacoelho@gmail.com <joaovictorgarciacoelho@gmail.com>
2023-05-16 20:58:34 -0300

    Configuring GlobalStyles

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,6 @@ import { Container, GlobalStyle, Home } from "./styles/global"
 import { Header } from "./components/Header/Header"
 import { SummaryTable } from "./components/SummaryTable/SummaryTable"
 
-import './styles/global.css'
 
 export function App() {
 
