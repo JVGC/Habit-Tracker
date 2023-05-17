a2f29f39702108269c7235e665ea1321da7c6cb6
joaovictorgarciacoelho@gmail.com <joaovictorgarciacoelho@gmail.com>
2023-05-17 07:47:40 -0300

    Starting HabitDay Popover Structure

--- frontend/src/components/HabitDay/HabitDay.tsx
+++ frontend/src/components/HabitDay/HabitDay.tsx
@@ -1,7 +1,26 @@
-import { Container } from "./styles";
+import * as Popover from '@radix-ui/react-popover'
 
+import { ProgressIndicator, ProgressRoot, PopoverArrow, PopoverContent, PopoverTrigger, SelectedDate, SelectedDay } from "./styles";
+
+const completed =  0
 export function HabitDay(){
+
+
   return (
-    <Container />
+    <Popover.Root>
+      <PopoverTrigger />
+      <Popover.Portal>
+        <PopoverContent>
+          <SelectedDay>Quarta</SelectedDay>
+          <SelectedDate>17/05</SelectedDate>
+
+          <ProgressRoot value={completed}>
+          <ProgressIndicator completed={completed}/>
+
+          </ProgressRoot>
+          <PopoverArrow height={8} width={16}/>
+        </PopoverContent>
+      </Popover.Portal>
+    </Popover.Root>
   )
 }
--- frontend/src/components/HabitDay/styles.ts
+++ frontend/src/components/HabitDay/styles.ts
@@ -1,6 +1,8 @@
+import * as Popover from '@radix-ui/react-popover'
+import * as Progress from '@radix-ui/react-progress';
 import { styled } from "styled-components";
 
-export const Container = styled.div`
+export const PopoverTrigger = styled(Popover.Trigger)`
   height: 2.5rem;
   width: 2.5rem;
 
@@ -8,4 +10,51 @@ export const Container = styled.div`
   border-width: 2px;
   border-color: #27272A;
   border-radius: 0.5rem;
+  cursor: pointer;
+`
+
+export const PopoverContent = styled(Popover.Content)`
+  min-width: 320px;
+  padding: 1.5rem;
+
+  border-radius: 1rem;
+  background-color: #18181B;
+  display: flex;
+  flex-direction: column;
+`
+
+export const PopoverArrow = styled(Popover.Arrow)`
+  fill: #18181B;
+`
+
+export const SelectedDay = styled.span`
+  font-weight: 600;
+  color: #A1A1AA;
+
+`
+
+export const SelectedDate = styled.span`
+  margin-top: 0.25rem;
+  font-weight: 800;
+  font-size: 1.875rem;
+  line-height: 1.25;
+`
+
+export const ProgressRoot = styled(Progress.Root)`
+  height: 0.75rem;
+  border-radius: 0.75rem;
+  background-color: #3F3F46;
+  width: 100%;
+  margin-top: 1rem;
+`
+
+export const ProgressIndicator = styled(Progress.Indicator)<{
+  completed: number
+}>`
+  height: 0.75rem;
+  border-radius: 0.75rem;
+
+  background-color: #7C3AED;
+
+  width: ${props => props.completed}%;
 `
