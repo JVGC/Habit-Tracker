0a412ff3b30ee2735e5b3e2d9429d9950e873935
joaovictorgarciacoelho@gmail.com <joaovictorgarciacoelho@gmail.com>
2023-05-17 17:40:50 -0300

    Fixing ListHabits completed logic

--- server/habits/views.py
+++ server/habits/views.py
@@ -2,8 +2,9 @@ from rest_framework import generics, views
 from rest_framework.request import Request
 from rest_framework.response import Response
 from .models import Habit
+from days.models import Day, DayHabit
 from .serializers import HabitSerializer
-from django.db.models import Sum
+from django.db.models import Sum, Q
 
 class AddNewHabit(generics.CreateAPIView):
     queryset = Habit.objects.all()
@@ -16,6 +17,15 @@ class ListHabits(views.APIView):
       requested_date = request.query_params.get('date', None)
       if requested_date:
         habits = Habit.getHabitsByDate(date=request.query_params['date']).annotate(completed=Sum("dayhabit__completed", default=False))
+        day = Day.objects.filter(date=request.query_params['date'])
+        if day:
+          dayHabits = DayHabit.objects.filter(day__id=day[0].id).all()
+          for habit in habits:
+            if not dayHabits.filter(habit__id=habit.id):
+              habit.completed = False
+        else:
+          for habit in habits:
+            habit.completed = False
       else:
         habits = Habit.objects.all()
       serializer = HabitSerializer(habits, many=True)
