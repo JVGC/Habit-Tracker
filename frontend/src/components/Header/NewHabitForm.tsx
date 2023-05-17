8f355ac24b4c0821fe4352917b2a916ea7c43a27
joaovictorgarciacoelho@gmail.com <joaovictorgarciacoelho@gmail.com>
2023-05-17 11:53:22 -0300

    Connecting CreateHabit to backend api

--- frontend/src/components/Header/NewHabitForm.tsx
+++ frontend/src/components/Header/NewHabitForm.tsx
@@ -1,9 +1,24 @@
 import { Check } from "phosphor-react";
 import { Form, TitleLabel, SubmitButton, TitleInput,  } from "./styles";
+import { FormEvent, useState } from "react";
+import { HabitService } from "../../services/HabitService";
+import dayjs from "dayjs";
 
 export function NewHabitForm(){
+
+  const [title, setTitle] = useState<string>('')
+
+  const createNewHabit = async (e: FormEvent) => {
+    e.preventDefault()
+    try{
+      const response = await HabitService.createNewHabit({title, startAt: dayjs(new Date()).format('YYYY-MM-DD')})
+      console.log(response)
+    }catch(err){
+      console.log(err)
+    }
+  }
   return (
-    <Form action="">
+    <Form onSubmit={createNewHabit} action="">
 
       <TitleLabel htmlFor="title">
         What are you willing to do?
@@ -14,6 +29,7 @@ export function NewHabitForm(){
         type="text"
         placeholder="Gym, drink water, meditation..."
         autoFocus
+        onChange={e => setTitle(e.target.value)}
       />
 
       <SubmitButton type="submit">
