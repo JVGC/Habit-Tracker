c4cc9f5b1e2f7894fd89e18192bb55e22847d6e1
joaovictorgarciacoelho@gmail.com <joaovictorgarciacoelho@gmail.com>
2023-05-25 19:59:15 -0300

    Adding List Habits Test

---
+++ server/habits/tests/test_list_habits.py
@@ -0,0 +1,124 @@
+# pylint: disable=missing-module-docstring
+# pylint: disable=missing-class-docstring
+# pylint: disable=missing-function-docstring
+
+
+from datetime import datetime
+from rest_framework.test import APIClient, APITestCase
+from habits.models import Habit
+from days.models import Day, DayHabit
+
+
+class ListHabitsTest(APITestCase):
+    def test_list_habits_no_habits_at_all(self):
+        client = APIClient()
+        response = client.get("/habits/")
+
+        self.assertEqual(len(response.data), 0)
+
+    def test_list_habits_all_habits_at_all(self):
+        habit_name = "habit1"
+        today = datetime.today().date()
+        start_at = datetime(today.year - 5, today.month, today.day).date()
+        Habit(name=habit_name, start_at=start_at).save()
+
+        habit_name = "habit2"
+        start_at = datetime(today.year - 4, today.month, today.day).date()
+        Habit(name=habit_name, start_at=start_at).save()
+
+        client = APIClient()
+        response = client.get("/habits/")
+
+        self.assertEqual(len(response.data), 2)
+
+    def test_list_habit_no_habits_for_that_day(self):
+        habit_name = "habit1"
+        today = datetime.today().date()
+        start_at = datetime(today.year + 1, today.month, today.day).date()
+        Habit(name=habit_name, start_at=start_at).save()
+
+        client = APIClient()
+        response = client.get("/habits/", {"date": today})
+
+        self.assertEqual(len(response.data), 0)
+
+    def test_list_habits_that_start_on_the_sent_date_day_obj_not_created(self):
+        habit_name = "habit1"
+        start_at = datetime.today().date()
+        habit = Habit.objects.create(name=habit_name, start_at=start_at)
+
+        client = APIClient()
+        response = client.get("/habits/", {"date": start_at})
+
+        self.assertEqual(len(response.data), 1)
+        self.assertEqual(response.data[0]["id"], habit.pk)
+        self.assertEqual(response.data[0]["completed"], False)
+
+    def test_list_habits_that_start_on_the_sent_date_day_obj_created_and_checked(self):
+        habit_name = "habit1"
+        start_at = datetime.today().date()
+
+        habit = Habit.objects.create(name=habit_name, start_at=start_at)
+        day = Day.objects.create(date=start_at)
+        DayHabit(habit=habit, day=day, completed=True).save()
+
+        client = APIClient()
+        response = client.get("/habits/", {"date": start_at})
+
+        self.assertEqual(len(response.data), 1)
+        self.assertEqual(response.data[0]["id"], habit.pk)
+        self.assertEqual(response.data[0]["completed"], True)
+
+    def test_list_habits_checked_day_after_but_not_on_sent_date(self):
+        habit_name = "habit1"
+        today = datetime.today().date()
+
+        habit = Habit.objects.create(name=habit_name, start_at=today)
+        day = Day.objects.create(
+            date=datetime(today.year + 1, today.month, today.day).date()
+        )
+        DayHabit(habit=habit, day=day, completed=True).save()
+
+        client = APIClient()
+        response = client.get("/habits/", {"date": today})
+
+        self.assertEqual(len(response.data), 1)
+        self.assertEqual(response.data[0]["id"], habit.pk)
+        self.assertEqual(response.data[0]["completed"], False)
+
+    def test_list_habits_checked_day_before_but_not_on_sent_date(self):
+        habit_name = "habit1"
+        today = datetime.today().date()
+
+        habit = Habit.objects.create(name=habit_name, start_at=today)
+        day = Day.objects.create(
+            date=datetime(today.year - 1, today.month, today.day).date()
+        )
+        DayHabit(habit=habit, day=day, completed=True).save()
+
+        client = APIClient()
+        response = client.get("/habits/", {"date": today})
+
+        self.assertEqual(len(response.data), 1)
+        self.assertEqual(response.data[0]["id"], habit.pk)
+        self.assertEqual(response.data[0]["completed"], False)
+
+    def test_list_habits_just_one_of_two_checked(self):
+        habit_name = "habit1"
+        start_at = datetime.today().date()
+
+        habit = Habit.objects.create(name=habit_name, start_at=start_at)
+        day = Day.objects.create(date=start_at)
+        DayHabit(habit=habit, day=day, completed=True).save()
+
+        habit_name = "habit2"
+        habit2 = Habit.objects.create(name=habit_name, start_at=start_at)
+
+        client = APIClient()
+        response = client.get("/habits/", {"date": start_at})
+
+        self.assertEqual(len(response.data), 2)
+        self.assertEqual(response.data[0]["id"], habit.pk)
+        self.assertEqual(response.data[0]["completed"], True)
+        self.assertEqual(response.data[1]["id"], habit2.pk)
+        self.assertEqual(response.data[1]["completed"], False)
