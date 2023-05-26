e686c5ad3d65107d31571f5cb6926ff9ef819c91
joaovictorgarciacoelho@gmail.com <joaovictorgarciacoelho@gmail.com>
2023-05-26 12:27:52 -0300

    Organizing list days use case code

--- server/days/use_cases/list_days.py
+++ server/days/use_cases/list_days.py
@@ -1,7 +1,6 @@
 """ List Days Use Case Class """
 
 from typing import TypedDict
-from django.db.models import Q, Count
 
 from days.serializers import DaySerializer
 from days.models.day import Day
@@ -13,18 +12,22 @@ ResponseData = TypedDict("response_data", {"status": int, "data": dict[str, str]
 class ListDaysUseCase:
     """List Days Use Case Implementation"""
 
+    def _get_days_with_completed_habits_count(self):
+        return (
+            Day.objects.all()
+            .annotate(completed=Day.count_completed_habits())
+            .order_by("date")
+        )
+
     def execute(self) -> ResponseData:
         """Return all Days ordered by date ascendent,
         and for each day, returns the number of completed habits
         and the total amount of habits to be completed.
         """
-        days = (
-            Day.objects.all()
-            .annotate(completed=Count("dayhabit", filter=Q(dayhabit__completed=True)))
-            .order_by("date")
-        )
+        days = self._get_days_with_completed_habits_count()
 
         day_serializer = DaySerializer(days, many=True)
         for day in day_serializer.data:
             day["total"] = len(Habit.get_habits_by_date(day["date"]))
+
         return ResponseData(status=200, data=day_serializer.data)
