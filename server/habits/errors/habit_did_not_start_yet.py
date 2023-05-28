a56f5e266c1015e86cc482ae86bc253f31581994
joaovictorgarciacoelho@gmail.com <joaovictorgarciacoelho@gmail.com>
2023-05-28 16:25:26 -0300

    Removing unnecessary UseCaseError Class

--- server/habits/errors/habit_did_not_start_yet.py
+++ server/habits/errors/habit_did_not_start_yet.py
@@ -1,12 +1,8 @@
 """ Habit Did not start yet error """
-from habit_tracker.interfaces import UseCaseError
 
 
-class HabitDidNotStartYet(UseCaseError):
+class HabitDidNotStartYet(BaseException):
     """Habit Did not start yet error"""
 
     def __init__(self) -> None:
-        message = "Habit didn't start yet"
-        status = "habit_did_not_start_yet"
-
-        super().__init__(message, status)
+        self.message = "Habit didn't start yet"
--- server/habits/errors/habit_does_not_exist.py
+++ server/habits/errors/habit_does_not_exist.py
@@ -1,12 +1,8 @@
 """Habit does not exist error"""
-from habit_tracker.interfaces import UseCaseError
 
 
-class HabitDoesNotExistError(UseCaseError):
+class HabitDoesNotExistError(BaseException):
     """Habit does not exist error"""
 
     def __init__(self) -> None:
-        message = "Habit does not exist"
-        status = "habit_does_not_exist"
-
-        super().__init__(message, status)
+        self.message = "Habit does not exist"
