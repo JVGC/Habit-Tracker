7d3a2fa2389968f1a81fa91e7c67da57e084bd6d
joaovictorgarciacoelho@gmail.com <joaovictorgarciacoelho@gmail.com>
2023-05-28 16:41:25 -0300

    Changing main django app name

--- server/django_root/asgi.py
+++ server/django_root/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for habit_tracker project.
+ASGI config for django_root project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "habit_tracker.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_root.settings")
 
 application = get_asgi_application()
