5997b4eddcc8bfd5f33c8b28b355b3b934cffd42
joaovictorgarciacoelho@gmail.com <joaovictorgarciacoelho@gmail.com>
2023-05-28 16:05:11 -0300

    List Date Habits as a child Class from UseCase Interface

--- server/habits/use_cases/list_date_habits.py
+++ server/habits/use_cases/list_date_habits.py
@@ -1,27 +1,26 @@
 """ List Habits for a Date Use Case Class """
-from typing import TypedDict
 from django.db.models import Sum
 from django.core.exceptions import ObjectDoesNotExist
+from rest_framework.exceptions import ReturnDict
 
+from habit_tracker.interfaces import UseCase
 from days.models import DayHabit, Day
 from habits.models import Habit
 from habits.serializers import HabitSerializer
 
 
-RequestData = TypedDict("request_data", {"date": str})
-
-
-class ListDateHabitsUseCase:
+class ListDateHabitsUseCase(UseCase):
     """ListDateHabits Use Case Implementation"""
 
-    def execute(self, data: RequestData):
-        """List all the habit for a given date"""
-        habits = Habit.get_habits_by_date(date=data["date"]).annotate(
+    @staticmethod
+    def execute(date: str = "") -> ReturnDict:
+        """List all the habits for a given date"""
+        habits = Habit.get_habits_by_date(date=date).annotate(
             completed=Sum("dayhabit__completed", default=False)
         )
         habits = HabitSerializer(habits, many=True).data
         try:
-            day = Day.objects.get(date=data["date"])
+            day = Day.objects.get(date=date)
             day_habits = DayHabit.objects.filter(day__id=day.pk)
             for habit in habits:
                 if not day_habits.filter(habit__id=habit["id"]):
--- server/habits/views.py
+++ server/habits/views.py
@@ -1,6 +1,5 @@
 """ Habits Django App Views """
 
-from typing import TypedDict, Any
 from rest_framework import generics, views
 from rest_framework.request import Request
 from rest_framework.response import Response
@@ -18,28 +17,19 @@ class AddNewHabit(generics.CreateAPIView):
     serializer_class = HabitSerializer
 
 
-IsValidResponse = TypedDict("is_valid_response", {"is_valid": bool, "data": Any})
-
-
 class ListHabits(views.APIView):
     """List Habits API View"""
 
-    def _validate(self, input_data):
+    def _validate(self, input_data) -> None:
+        """Validate Request params and body.
+        Raises Exception if input is not valid
+        """
         day_serializer = DaySerializer(data={"date": input_data["date"]})  # type: ignore
-        is_date_valid = day_serializer.is_valid()
-        if not is_date_valid:
-            return IsValidResponse(
-                is_valid=False,
-                data={"status": 400, "data": day_serializer.errors},
-            )
-        return IsValidResponse(is_valid=True, data=None)
+        day_serializer.is_valid(raise_exception=True)
 
     def get(self, request: Request) -> Response:
         """GET Request for List Habits API View"""
-        is_input_valid = self._validate(request.query_params)
-        if not is_input_valid["is_valid"]:
-            response_data = is_input_valid["data"]
-            return Response(status=response_data["status"], data=response_data["data"])
+        self._validate(request.query_params)
 
-        habits = ListDateHabitsUseCase().execute({"date": request.query_params["date"]})
+        habits = ListDateHabitsUseCase.execute(request.query_params["date"])
         return Response(data=habits)
