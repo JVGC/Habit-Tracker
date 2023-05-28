bc976fb3ca3e9af42e8160c7674fb28781e0a945
joaovictorgarciacoelho@gmail.com <joaovictorgarciacoelho@gmail.com>
2023-05-28 11:07:34 -0300

    Creating Serializer folders for the Habit App

---
+++ server/habits/serializers/__init__.py
@@ -0,0 +1,1 @@
+from .habit_serializer import HabitSerializer
--- server/habits/serializers/habit_serializer.py
+++ server/habits/serializers/habit_serializer.py
@@ -1,6 +1,6 @@
 """ Habits App Serializers """
 from rest_framework import serializers
-from .models import Habit
+from habits.models import Habit
 
 
 class HabitSerializer(serializers.ModelSerializer):
